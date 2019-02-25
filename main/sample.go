5fbe7fae779c9947105bdc0d5f8d42df8a5969f8
John Carlo Salter <juuncaerlum@gmail.com>
2019-02-25 16:42:55 -0500

    add: example of how to run the parser.

---
+++ main/sample.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/metrumresearchgroup/rcmdparser/rcmdparser"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+// This function provides a sample of how the rcmdparser might be used.
+// Assume that the package check functionality has been run for all of your packages,
+// and the results have been saved to ../rcmdparser/testdata
+func main() {
+	var r io.Reader
+	var byteData []byte
+
+	wd, _ := os.Getwd()
+	fmt.Sprintf("Working directory: %s", wd)
+	file, _ := os.Open("rcmdparser/testdata/testwarningerror.Rcheck/00check.log")
+	r = bufio.NewReader(file)
+	byteData, _  = ioutil.ReadAll(r)
+	output := rcmdparser.ParseTestLog(byteData)
+	fmt.Println(fmt.Sprintf("========Output of Parser=========\n%s\n=========END=========", output))
+
+}
